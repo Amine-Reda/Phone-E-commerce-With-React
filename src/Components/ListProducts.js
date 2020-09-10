f818b42c537e50ca2efc03b96d1d0c5ece97c72f
amine reda <amine.reda037@gmail.com>
2020-09-10 13:01:47 +0100

    add listProduct

--- src/Components/ListProducts.js
+++ src/Components/ListProducts.js
@@ -1,17 +1,109 @@
 import React from "react";
 import "./ListProducts.css";
 import Product from "./Product";
+import { Row, Col, Container } from "react-bootstrap";
 import { storeProducts } from "../data";
 function ListProducts() {
   return (
     <div className="listProducts">
-      {storeProducts.map((product) => (
-        <Product
-          img={product.img}
-          title={product.title}
-          price={product.price}
-        />
-      ))}
+      <Container>
+        <Row>
+          <Col md={1} xs={1} lg={1}></Col>
+          <Col md={6} xs={8} lg={2}>
+            <Product
+              title={storeProducts[0].title}
+              price={storeProducts[0].price}
+              img={storeProducts[0].img}
+            />
+          </Col>
+          <Col md={6} xs={8} lg={2}>
+            <Product
+              title={storeProducts[1].title}
+              price={storeProducts[1].price}
+              img={storeProducts[1].img}
+            />
+          </Col>
+          <Col md={6} xs={8} lg={2}>
+            <Product
+              title={storeProducts[2].title}
+              price={storeProducts[2].price}
+              img={storeProducts[2].img}
+            />
+          </Col>
+          <Col md={6} xs={8} lg={2}>
+            <Product
+              title={storeProducts[3].title}
+              price={storeProducts[3].price}
+              img={storeProducts[3].img}
+            />
+          </Col>
+          <Col md={1} xs={1} lg={1}></Col>
+        </Row>
+        <Row>
+          <Col md={1} xs={1} lg={1}></Col>
+          <Col md={6} xs={8} lg={2}>
+            <Product
+              title={storeProducts[4].title}
+              price={storeProducts[4].price}
+              img={storeProducts[4].img}
+            />
+          </Col>
+          <Col md={6} xs={8} lg={2}>
+            <Product
+              title={storeProducts[5].title}
+              price={storeProducts[5].price}
+              img={storeProducts[5].img}
+            />
+          </Col>
+          <Col md={6} xs={8} lg={2}>
+            <Product
+              title={storeProducts[6].title}
+              price={storeProducts[6].price}
+              img={storeProducts[6].img}
+            />
+          </Col>
+          <Col md={6} xs={8} lg={2}>
+            <Product
+              title={storeProducts[7].title}
+              price={storeProducts[7].price}
+              img={storeProducts[7].img}
+            />
+          </Col>
+          <Col md={1} xs={1} lg={1}></Col>
+        </Row>
+        <Row>
+          <Col md={1} xs={1} lg={1}></Col>
+          <Col md={6} xs={8} lg={2}>
+            <Product
+              title={storeProducts[0].title}
+              price={storeProducts[0].price}
+              img={storeProducts[0].img}
+            />
+          </Col>
+          <Col md={6} xs={8} lg={2}>
+            <Product
+              title={storeProducts[1].title}
+              price={storeProducts[1].price}
+              img={storeProducts[1].img}
+            />
+          </Col>
+          <Col md={6} xs={8} lg={2}>
+            <Product
+              title={storeProducts[2].title}
+              price={storeProducts[2].price}
+              img={storeProducts[2].img}
+            />
+          </Col>
+          <Col md={6} xs={8} lg={2}>
+            <Product
+              title={storeProducts[3].title}
+              price={storeProducts[3].price}
+              img={storeProducts[3].img}
+            />
+          </Col>
+          <Col md={1} xs={1} lg={1}></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
